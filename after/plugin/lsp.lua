1472dbd74da4788a611be3cabd5073ae08f9a152
burritoOverflow <47379530+burritoOverflow@users.noreply.github.com>
2023-03-21 22:59:29 -0400

    Additional LSP bindings

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -32,9 +32,10 @@ lsp.setup_nvim_cmp({
 	mapping = cmp_mappings
 })
 
-lsp.on_attach(function(client, bufnr)
+lsp.on_attach(function(_, bufnr)
 	local opts = {buffer = bufnr, remap = false}
-
+    vim.keymap.set("n", "K", function() vim.lsp.hover() end, opts)
+    vim.keymap.set("n", "<C-k>", function() vim.lsp.signature_help() end, opts)
 	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
 	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
 	vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
